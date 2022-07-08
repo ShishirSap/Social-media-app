5a40de3a3ddea7ec52006ce0d1956b122aeeae5c
Shishir Sapkota <shishir.sap83@gmail.com>
2022-07-08 10:29:10 +0545

    second

--- social_book/social_book/settings.py
+++ social_book/social_book/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=g-k9^67!9lau+0u7yx)ye1**1r9^8_!n6qdx&)zu$yqs*&vn$'
+from decouple import config
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
